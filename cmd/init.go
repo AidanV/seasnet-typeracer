7189cc860760bc146bead231d4875641f38c9575
AidanV <aidanvanduyne@gmail.com>
2024-08-03 12:54:30 -0700

    Restructured project

---
+++ cmd/init.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func InitialModel() model {
+	return model{
+		// Our to-do list is a grocery list
+		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+
+		// A map which indicates which choices are selected. We're using
+		// the  map like a mathematical set. The keys refer to the indexes
+		// of the `choices` slice, above.
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
+}
