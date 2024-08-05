98fac9b00dca16e43a08101bafa946d23cb9dc5f
AidanV <aidanvanduyne@gmail.com>
2024-08-05 16:00:01 -0700

    Just udp connection

--- main.go
+++ main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	// "fmt"
+	// "os"
+	"sync"
 	client "typeracer/cmd/client"
 	server "typeracer/cmd/server"
-	ui "typeracer/cmd/ui"
-
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/muesli/termenv"
+	// ui "typeracer/cmd/ui"
+	// tea "github.com/charmbracelet/bubbletea"
+	// "github.com/muesli/termenv"
 )
 
 func main() {
-	go server.Server()
-	go client.PostToServer()
-	p := tea.NewProgram(ui.InitialModel(termenv.ANSI256, termenv.ANSIWhite), tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
-	}
+
+	var wg sync.WaitGroup
+	go func() {
+		server.Server()
+		wg.Done()
+	}()
+	go func() {
+		client.PostToServer()
+		wg.Done()
+	}()
+	wg.Add(2)
+	// p := tea.NewProgram(ui.InitialModel(termenv.ANSI256, termenv.ANSIWhite), tea.WithAltScreen())
+	// if _, err := p.Run(); err != nil {
+	// 	fmt.Printf("Alas, there's been an error: %v", err)
+	// 	os.Exit(1)
+	// }
+	wg.Wait()
 }
